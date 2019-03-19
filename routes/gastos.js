5c7af01a5edc2570d682ea93ae38988b56eb01ec
Luis Enrique Oviedo <le.oviedo@gmail.com>
2019-03-19 09:37:38 -0300

    21090319
    cambios en datos de gastos

--- routes/gastos.js
+++ routes/gastos.js
@@ -144,12 +144,12 @@ app.get('/', function(req, res, next) {
         var sql_con ="";
         if(user == "cibanez" || user == "prueba")
         {   sql_con = "SELECT t1.id,t1.fecha,t1.monto,t1.exentas,t1.iva_10,t1.iva_5,t1.gasto_real,t1.concepto,t1.fact_condicion, t1.proveedor,t1.fact_nro, t1.encargado,t1.codigo, " + 
-        "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 inner join ot t2 on t2.ot_nro = t1.nro_ot " + 
+        "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 left join ot t2 on t2.ot_nro = t1.nro_ot " + 
         "WHERE t1.usuario_insert in ('cibanez','prueba') or t1.tipo = 'NO_CONF' order by t1.fecha desc";}
         else
         //traemos los datos (OBRA y CLIENTE) de la OT asociada a ese gasto.
         {   sql_con = "select * from (SELECT t1.id,t1.fecha,t1.monto,t1.exentas,t1.iva_10,t1.iva_5,t1.gasto_real,t1.concepto,t1.fact_condicion, t1.proveedor,t1.fact_nro, t1.encargado,t1.codigo, " + 
-        "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 inner join ot t2 on t2.ot_nro = t1.nro_ot order by t1.fecha desc)";}
+        "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 left join ot t2 on t2.ot_nro = t1.nro_ot order by t1.fecha desc)";}
         req.getConnection(function(error, conn) {
             conn.query(sql_con,function(err, rows) {
                 if (err) {
