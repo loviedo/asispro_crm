d727962128b4d2c82b4bcd66c6944eae3b0291cc
Luis Enrique Oviedo <le.oviedo@gmail.com>
2019-05-05 14:27:06 -0400

    20190505
    cambios en mano de obra

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 var config = {
     database: {
-        host:      'localhost', // database host
+        //host:      'localhost', // database host debug
+        host:      '216.70.112.44', // database host
         user:       'root', //username del pass
         password: 'Super123', //pass
         port:       3306, // default MySQL port
