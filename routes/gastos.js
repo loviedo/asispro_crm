df0a031c29dc19192a759e457b7ebf92c01b0829
Luis Enrique Oviedo <le.oviedo@gmail.com>
2020-01-13 22:44:38 -0300

    cambios 3
    //probar https://www.npmjs.com/package/excel4node/v/0.2.20

--- routes/gastos.js
+++ routes/gastos.js
@@ -150,7 +150,7 @@ app.get('/historico', function(req, res, next) {
 
         //cada usuario puede ver solamente su carga, y solamente los administradores pueden ver todo.
         //verificar si los usuarios bajo karen pueden ver los tipos de carga "NO CONFIDENCIALES".
-        if(user == "cibanez" || user == "prueba" || user == "jlopez" || user == "jguerrero" || user == "fduarte" || user == "ogonzalez")
+        if(user == "rsanabria" || user == "cibanez" || user == "prueba" || user == "jlopez" || user == "jguerrero" || user == "fduarte" || user == "ogonzalez")
         {sql_con = "SELECT t1.id,t1.fecha,t1.monto,t1.exentas,t1.iva_10,t1.iva_5,t1.gasto_real,t1.concepto,t1.fact_condicion, t1.proveedor,t1.fact_nro, t1.encargado,t1.codigo, " + 
         "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 left join ot t2 on t2.ot_nro = t1.nro_ot " + 
         "left join cajas c1 on c1.id = t1.id_caja " +
@@ -221,15 +221,15 @@ app.get('/', function(req, res, next) {
         var sql_con ="";
         var sql_lis = "";
         //como estaba originalmente
-        /*if(user == "cibanez" || user == "prueba")
+        /*if(user == "rsanabria" || user == "cibanez" || user == "prueba")
         {   sql_con = "SELECT t1.id,t1.fecha,t1.monto,t1.exentas,t1.iva_10,t1.iva_5,t1.gasto_real,t1.concepto,t1.fact_condicion, t1.proveedor,t1.fact_nro, t1.encargado,t1.codigo, " + 
         "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 left join ot t2 on t2.ot_nro = t1.nro_ot " + 
-        "WHERE t1.usuario_insert in ('cibanez','prueba') or t1.tipo = 'NO_CONF' order by t1.fecha desc";}
+        "WHERE t1.usuario_insert in ('rsanabria','cibanez','prueba') or t1.tipo = 'NO_CONF' order by t1.fecha desc";}
         else*/
 
         //cada usuario puede ver solamente su carga, y solamente los administradores pueden ver todo.
         //verificar si los usuarios bajo karen pueden ver los tipos de carga "NO CONFIDENCIALES".
-        if(user == "cibanez" || user == "prueba" || user == "jlopez" || user == "jguerrero" || user == "fduarte" || user == "ogonzalez")
+        if(user == "rsanabria" || user == "cibanez" || user == "prueba" || user == "jlopez" || user == "jguerrero" || user == "fduarte" || user == "ogonzalez")
         {sql_con = "SELECT t1.id,t1.fecha,t1.monto,t1.exentas,t1.iva_10,t1.iva_5,t1.gasto_real,t1.concepto,t1.fact_condicion, t1.proveedor,t1.fact_nro, t1.encargado,t1.codigo, " + 
         "t1.nro_ot, t1.imputado, t1.usuario_insert, t1.origen_pago, t1.tipo, t1.id_proveedor, t2.ot_nro, t2.cliente, t2.obra FROM gastos t1 left join ot t2 on t2.ot_nro = t1.nro_ot " + 
         "left join cajas c1 on c1.id = t1.id_caja " +
